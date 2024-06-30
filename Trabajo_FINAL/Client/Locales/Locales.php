f61a409c6d892ba8650c40ac9cfce92ceadf692c
Shakir <Shakirchaya.dev@gmail.com>
2024-06-30 19:37:26 -0300

    Agregando cosas respecto a la accesibilidad

--- Trabajo_FINAL/Client/Locales/Locales.php
+++ Trabajo_FINAL/Client/Locales/Locales.php
@@ -2,7 +2,7 @@
   session_start();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,9 +16,9 @@
     integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
     crossorigin="anonymous"></script>
     <link rel="stylesheet" href="Locales.css">
-    <link rel="stylesheet" href="../../Pie_de_Pagina/footer.css">
+    <link rel="stylesheet" href="../../Pie_De_Pagina/footer.css">
     <link rel="stylesheet" href="../../Barra_Navegacion/Bar-style.css">
-    <title>Rosario Shopping Center Locales</title>
+    <title>Locales | Rosario Shopping Center</title>
 </head>
 <body>
   <!-- Módulo de barra de navegación -->
@@ -28,86 +28,87 @@
   ?>
 
   <!-- Título de Locales-->
+  <main>
+      <div class="lineas_title">Locales</div>
 
-    <div class="lineas_title">Locales</div>
+    <!-- Barra de búsqueda de locales -->
+        <form class="filtrado_locales" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
+          <label class="search_label" for="select_parametro"><b>Búsqueda de local:</b>
+            <select name="parametro" id="select_parametro" class="form-search__select" aria-label="Seleccion de opciones">
+                <option value="nombreLocal">Por nombre</option>
+                <option value="ubicacionLocal">Por ubicación</option>
+                <option value="rubroLocal">Por rubro</option>
+                <option value="codUsuario">Por código de Dueño</option>
+            </select>
+          </label>
+          <input id= "lupa_local" type="text" class="busqueda_local" name="busqueda" placeholder="¿Qué desea buscar?" title="Busqueda de local">
+          <label for="enviar_busqueda" class="label_busqueda">
+            <img class="lupa_busqueda" src="../../Imagenes-Videos/lupa.png" alt="Lupa de busqueda"><input type="submit" class="lupa_input" id="enviar_busqueda" name="busqued" title="Boton de busqueda">
+          </label>
+        </form>
 
-  <!-- Barra de búsqueda de locales -->
-      <form class="filtrado_locales" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
-        <label class="search_label" for="select_parametro"><b>Búsqueda de local:</b>
-          <select name="parametro" id="select_parametro" class="form-search__select">
-              <option value="nombreLocal">Por nombre</option>
-              <option value="ubicacionLocal">Por ubicación</option>
-              <option value="rubroLocal">Por rubro</option>
-              <option value="codUsuario">Por código de Dueño</option>
-          </select>
-        </label>
-        <input id= "lupa_local" type="text" class="busqueda_local" name="busqueda" placeholder="¿Qué desea buscar?">
-        <label for="enviar_busqueda" class="label_busqueda">
-          <img class="lupa_busqueda" src="../../Imagenes-Videos/lupa.png" alt="lupa de busqueda"><input type="submit" class="lupa_input" id="enviar_busqueda" name="busqued">
-        </label>
-      </form>
+      <div class="iteracion">  
+        <?php
+        include("../../database.php");
+          if(empty($_POST["busqueda"])){
+            $sql = "SELECT * FROM locales";
+            $result = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_assoc($result)){
+                  $URL= "../Promociones/Promociones.php" . "?Local={$row["nombreLocal"]}";
+                    echo"
+                        <div class='container'>
+                              <div class ='local_data'> Local {$row['codLocal']} <br> {$row['ubicacionLocal']} <br> {$row['rubroLocal']} </div>
+                              <div class =  'base-div'>
+                                <p class = 'data'> {$row['nombreLocal']}</p>
+                              </div>
+                              <div class = 'hover-div'>
+                                <a class='link_promociones' href='$URL' aria-label='Promociones' title='Promociones'>Promociones</a>
+                              </div>
+                        </div>";
+                };
+            }
+            else{
+              echo"
+                <div class = 'error_box'>
+                  <p class = 'error'>No hay Locales</p>
+                </div>
 
-    <div class="iteracion">  
-      <?php
-      include("../../database.php");
-        if(empty($_POST["busqueda"])){
-          $sql = "SELECT * FROM locales";
-          $result = mysqli_query($conn, $sql);
-          if(mysqli_num_rows($result) > 0){
-              while($row = mysqli_fetch_assoc($result)){
+              ";
+            }
+          }
+          else{
+            $parame = $_POST["parametro"];
+            $busqueda = $_POST["busqueda"];
+            $sql = "SELECT * FROM locales WHERE $parame LIKE '$busqueda%'";
+            $resp = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($resp) > 0){
+              while($row = mysqli_fetch_assoc($resp)){
                 $URL= "../Promociones/Promociones.php" . "?Local={$row["nombreLocal"]}";
                   echo"
                       <div class='container'>
-                            <div class ='local_data'> Local {$row['codLocal']} <br> {$row['ubicacionLocal']} <br> {$row['rubroLocal']} </div>
-                            <div class =  'base-div'>
+                            <div class ='local_data'> Local {$row['codLocal']} <br> ubi {$row['ubicacionLocal']} <br> {$row['rubroLocal']} </div>
+                            <div class = 'base-div'>
                               <p class = 'data'> {$row['nombreLocal']}</p>
                             </div>
                             <div class = 'hover-div'>
-                              <a class='link_promociones' href='$URL'>Promociones</a>
+                              <a class='link_promociones' href='$URL' aria-label='Promociones' title='Promociones'>Promociones</a>
                             </div>
                       </div>";
               };
-          }
-          else{
-            echo"
-              <div class = 'error_box'>
-                <p class = 'error'>No hay Locales</p>
-              </div>
-            
-            ";
-          }
-        }
-        else{
-          $parame = $_POST["parametro"];
-          $busqueda = $_POST["busqueda"];
-          $sql = "SELECT * FROM locales WHERE $parame LIKE '$busqueda%'";
-          $resp = mysqli_query($conn, $sql);
-          if(mysqli_num_rows($resp) > 0){
-            while($row = mysqli_fetch_assoc($resp)){
-              $URL= "../Promociones/Promociones.php" . "?Local={$row["nombreLocal"]}";
-                echo"
-                    <div class='container'>
-                          <div class ='local_data'> Local {$row['codLocal']} <br> ubi {$row['ubicacionLocal']} <br> {$row['rubroLocal']} </div>
-                          <div class = 'base-div'>
-                            <p class = 'data'> {$row['nombreLocal']}</p>
-                          </div>
-                          <div class = 'hover-div'>
-                            <a class='link_promociones' href='$URL'>Promociones</a>
-                          </div>
-                    </div>";
-            };
-          }
-          else{
-            echo"
-              <div class = 'error_box'>
-                <p class = 'error'>No se encontro lo que buscaste</p>
-              </div>
-            
-            ";
-          }
-        };
-      ?>
-    </div>
+            }
+            else{
+              echo"
+                <div class = 'error_box'>
+                  <p class = 'error'>No se encontro lo que buscaste</p>
+                </div>
+
+              ";
+            }
+          };
+        ?>
+      </div>
+  </main>
     <?php
       include("../../Pie_De_Pagina/footer.php");
     ?>
--- Trabajo_FINAL/Home-UNR/index.php
+++ Trabajo_FINAL/Home-UNR/index.php
@@ -31,18 +31,17 @@ $_SESSION["tipoUsuario"] = "UNR";
         <p class="shopping-description p-delete">Bienvenidos a Rosario Shopping Center.</p>
         <p class="shopping-description">Moda, cine, gastronomía y mucho más! Descubrí el corazón de Rosario.</p>
         <div class="contenedor-flecha">
-          <a href="#locales" class="enlace-flecha">
-            <p class="flecha">
-              <svg data-bbox="20 59.5 160 81.001" viewBox="0 0 200 200" height="100" width="100" xmlns="http://www.w3.org/2000/svg" data-type="shape">
-                <g>
-                  <!-- Añadimos el círculo -->
-                  <circle cx="100" cy="100" r="75" stroke="white" stroke-width="5" fill="none" />
-                  <g transform="rotate(180 70 70) scale(0.4)">
-                    <path d="M177.687 128.054L105.35 61.402a7.205 7.205 0 0 0-5.35-1.886 7.198 7.198 0 0 0-5.349 1.886l-72.338 66.652a7.165 7.165 0 0 0-.407 10.138 7.172 7.172 0 0 0 5.283 2.309c1.743 0 3.49-.629 4.872-1.902L100 75.999l67.939 62.598a7.197 7.197 0 0 0 10.155-.406 7.163 7.163 0 0 0-.407-10.137z" fill="white"></path>
-                  </g>
-                </g>
-              </svg>
-            </p>
+          <a href="#locales" class="enlace-flecha" aria-label="Ir a la sección de locales" title="Ir a la sección de locales" style="outline: 2px dashed #000;">
+              <p class="flecha">
+                  <svg data-bbox="20 59.5 160 81.001" viewBox="0 0 200 200" height="100" width="100" xmlns="http://www.w3.org/2000/svg" data-type="shape">
+                      <g>
+                          <circle cx="100" cy="100" r="75" stroke="white" stroke-width="5" fill="none" />
+                          <g transform="rotate(180 70 70) scale(0.4)">
+                              <path d="M177.687 128.054L105.35 61.402a7.205 7.205 0 0 0-5.35-1.886 7.198 7.198 0 0 0-5.349 1.886l-72.338 66.652a7.165 7.165 0 0 0-.407 10.138 7.172 7.172 0 0 0 5.283 2.309c1.743 0 3.49-.629 4.872-1.902L100 75.999l67.939 62.598a7.197 7.197 0 0 0 10.155-.406 7.163 7.163 0 0 0-.407-10.137z" fill="white"></path>
+                          </g>
+                      </g>
+                  </svg>
+              </p>
           </a>
         </div>
       </div>
@@ -153,11 +152,11 @@ $_SESSION["tipoUsuario"] = "UNR";
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+        <span class="visually-hidden">Anterior</span>
       </button>
       <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <span class="visually-hidden">Siguiente</span>
       </button>
     </div>
   </section>
