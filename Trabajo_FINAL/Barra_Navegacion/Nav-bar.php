b8d95ddbadb930158484d0f1df8b5fccfa543ae4
SantiKellem <santikellemberger@gmail.com>
2024-06-30 13:05:03 -0300

    Corrigiendo errores de un commit anterior

--- Trabajo_FINAL/Barra_Navegacion/Nav-bar.php
+++ Trabajo_FINAL/Barra_Navegacion/Nav-bar.php
@@ -8,7 +8,7 @@
           if($_SESSION["tipoUsuario"] == "Cliente"){
     ?>
             <div class="navbar-style">
-                <a class="navbar-brand" href="../Home/Home.php"><img title="Inicio" class="icon" src="../../Imagenes-Videos/bolsas-de-compra.png" alt="Inicio"></a>
+                <a class="navbar-brand" href="../../Client/Home/Home.php"><img title="Inicio" class="icon" src="../../Imagenes-Videos/bolsas-de-compra.png" alt="Inicio"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -16,16 +16,16 @@
             <div class="collapse navbar-collapse" id="navbarScroll">
                 <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                   <li class="nav-item">
-                    <a class="nav-link active locales-active" aria-current="page" href="../Locales/Locales.php">Locales</a>
+                    <a class="nav-link active locales-active" aria-current="page" href="../../Client/Locales/Locales.php">Locales</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../Promociones/Promociones.php">Promociones</a>
+                    <a class="nav-link active" aria-current="page" href="../../Client/Promociones/Promociones.php">Promociones</a>
                   </li>
                   <li class="nav-item dropdown">
-                    <a class="nav-link active" aria-current="page" href="../Novedades/Novedades.php">Novedades</a>
+                    <a class="nav-link active" aria-current="page" href="../../Client/Novedades/Novedades.php">Novedades</a>
                   </li>
                   <li class="nav-item dropdown">
-                    <a class="nav-link active" aria-current="page" href="../Home/Home.php#AboutUs">Sobre Nosotros</a>
+                    <a class="nav-link active" aria-current="page" href="../../Client/Home/Home.php#AboutUs">Sobre Nosotros</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#Contacto">Contacto</a>
@@ -33,7 +33,10 @@
                 </ul>
                 <form class="d-flex align-items-center form-style" role="search" method="post">
                   <li class="nav-item dropdown list-item">
-                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><img class="avatar-style icon" src="../../Imagenes-Videos/avatar.png" alt="Logo de usuario"><?php echo"{$_SESSION['tipoUsuario']}";?></a>
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><img class="avatar-style icon" src="../../Imagenes-Videos/avatar.png" alt="Logo de usuario"><?php echo"{$_SESSION['tipoUsuario']}";?></a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="../../Perfil/perfil_pag/perfil.php">Ver Perfil</a></li>
+                    </ul>
                   </li>
                   <a class="btn btn-outline-danger log_out" href="../../inicio_de_sesion/inicio_sesion.php"><img class="icon cerrar-sesion btn-delete" src="../../Imagenes-Videos/Cerrar-sesion.png" alt="Imagen de puerta para cerrar sesion">Cerrar Sesion</a>
                 </form>
@@ -116,7 +119,7 @@
                       <a class="nav-link active" aria-current="page" href="#Contacto">Contacto</a>
                     </li>
                   </ul>
-                  <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])   ?>" method="post" class="d-flex align-items-center form-style" role="search">
+                  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])   ?>" method="post" class="d-flex align-items-center form-style" role="search">
                       <input class="btn btn-outline-success" name="Iniciar-Sesion" value="Iniciar Sesión" type="submit">
                       <input class="btn btn-outline-success success" name="Registrarse" value="Registrarse" type="submit">
                   </form>
@@ -143,7 +146,7 @@
         if($_SESSION["tipoUsuario"] == "Dueño de local"){
     ?>
             <div class="navbar-style">
-                <a class="navbar-brand" href="../Home/Home.php"><img title="Inicio" class="icon" src="../../Imagenes-Videos/bolsas-de-compra.png" alt="Inicio"></a>
+                <a class="navbar-brand" href="../../Owner/Home/Home.php"><img title="Inicio" class="icon" src="../../Imagenes-Videos/bolsas-de-compra.png" alt="Inicio"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -151,15 +154,18 @@
             <div class="collapse navbar-collapse" id="navbarScroll">
                 <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                   <li class="nav-item">
-                    <a class="nav-link active locales-active" aria-current="page" href="../Promociones/Promociones.php">Promociones</a>
+                    <a class="nav-link active locales-active" aria-current="page" href="../../Owner/Promociones/Promociones.php">Promociones</a>
                   </li>
                   <li class="nav-item dropdown">
-                    <a class="nav-link active" aria-current="page" href="../Reportes/Reporte-Usos.php">Reportes de Promociones</a>
+                    <a class="nav-link active" aria-current="page" href="../../Owner/Reportes/Reporte-Usos.php">Reportes de Promociones</a>
                   </li>
                 </ul>
                 <form class="d-flex align-items-center form-style" role="search" method="post">
                   <li class="nav-item dropdown list-item">
-                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><img class="avatar-style icon" src="../../Imagenes-Videos/avatar.png" alt="Logo de Usuario"><?php echo"{$_SESSION['tipoUsuario']}";?></a>
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><img class="avatar-style icon" src="../../Imagenes-Videos/avatar.png" alt="Logo de Usuario"><?php echo"{$_SESSION['tipoUsuario']}";?></a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="../../Perfil/perfil_pag/perfil.php">Ver Perfil</a></li>
+                    </ul>
                   </li>
                   <a class="btn btn-outline-danger log_out" href="../../inicio_de_sesion/inicio_sesion.php"><img class="icon cerrar-sesion btn-delete" src="../../Imagenes-Videos/Cerrar-sesion.png" alt="Imagen de puerta para cerrar sesion">Cerrar Sesion</a>
                 </form>
@@ -175,7 +181,7 @@
           if($_SESSION["tipoUsuario"] == "Administrador"){
       ?>
                   <div class="navbar-style">
-                    <a class="navbar-brand" href="../home/home_page_admin.php"><img title="Inicio" class="icon" src="../../Imagenes-Videos/bolsas-de-compra.png" alt="Inicio"></a>
+                    <a class="navbar-brand" href="../../admin/home/home_page_admin.php"><img title="Inicio" class="icon" src="../../Imagenes-Videos/bolsas-de-compra.png" alt="Inicio"></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                     </button>
@@ -183,31 +189,34 @@
                   <div class="collapse navbar-collapse" id="navbarScroll">
                     <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                       <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../locales_menu/admin_locales.php">Locales</a>
+                        <a class="nav-link active" aria-current="page" href="../../admin/locales_menu/admin_locales.php">Locales</a>
                       </li>
                       <li class="nav-item dropdown list-item">
                         <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Usuarios</a>
                         <ul class="dropdown-menu">
-                          <li><a class="dropdown-item" href="../users_menu/admin_users.php">Usuarios Registrados</a></li>
-                          <li><a class="dropdown-item" href="../users_menu/admin_owner.php">Validar/Denegar Cuentas de Dueño</a></li>
+                          <li><a class="dropdown-item" href="../../admin/users_menu/admin_users.php">Usuarios Registrados</a></li>
+                          <li><a class="dropdown-item" href="../../admin/users_menu/admin_owner.php">Validar/Denegar Cuentas de Dueño</a></li>
                         </ul>
                       </li>
                       <li class="nav-item dropdown">
-                        <a class="nav-link active" aria-current="page" href="../novedades_menu/admin_nov.php">Novedades</a>
+                        <a class="nav-link active" aria-current="page" href="../../admin/novedades_menu/admin_nov.php">Novedades</a>
                       </li>
                       <li class="nav-item dropdown list-item">
                         <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Promociones</a>
                         <ul class="dropdown-menu">
-                          <li><a class="dropdown-item" href="../promociones_menu/admin_lista_promo.php">Promociones Cargadas</a></li>
-                          <li><a class="dropdown-item" href="../promociones_menu/admin_promo.php">Aceptar/Rechazar Promociones</a></li>
+                          <li><a class="dropdown-item" href="../../admin/promociones_menu/admin_lista_promo.php">Promociones Cargadas</a></li>
+                          <li><a class="dropdown-item" href="../../admin/promociones_menu/admin_promo.php">Aceptar/Rechazar Promociones</a></li>
                         </ul>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../uso_promociones/uso_promo.php">Uso de Promociones</a>
+                        <a class="nav-link active" aria-current="page" href="../../admin/uso_promociones/uso_promo.php">Uso de Promociones</a>
                       </li>
                     </ul>
                     <span class="nav-item dropdown list-item">
                       <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><img class="avatar-style icon" src="../../Imagenes-Videos/avatar.png" alt="Logo de usuario">Administrador</a>
+                      <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="../../Perfil/perfil_pag/perfil.php">Ver Perfil</a></li>
+                      </ul>
                     </span>
                     <a class="btn btn-outline-danger log_out" href="../../inicio_de_sesion/inicio_sesion.php"><img class="icon cerrar-sesion btn-delete" src="../../Imagenes-Videos/Cerrar-sesion.png" alt="Imagen de puerta para cerrar sesion">Cerrar Sesion</a>
                   </div>
